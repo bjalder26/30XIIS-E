0c4dfa5334ccd0117d38bd7d6fe77205831f64e8
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2026-05-14 23:55:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1485,7 +1485,7 @@ function powerOn() {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("service-worker.js")
       .then(reg => console.log("Service Worker registered", reg))
       .catch(err => console.error("Service Worker failed", err));
   });
