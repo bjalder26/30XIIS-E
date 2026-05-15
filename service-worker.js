2c3ddaa81d65bb75cf096fb54267d16795e3a415
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2026-05-14 22:52:05 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "30XIIS-E-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/index.css",
+  "/index.js"
+];
+
+// Install: cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch: serve cached content if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
